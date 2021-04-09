6cfdfee0e7ae6720f4cf04d3ae97d5bb4d6c44df
EgonAxel <egonaxelsson@hotmail.se>
2021-04-09 15:34:01 +0200

    Öl i mitten av kort

--- Screens/Home.js
+++ Screens/Home.js
@@ -11,43 +11,45 @@ function Home() {
 
     useEffect(() => {
 
-        fetch("http://127.0.0.1:8000/beer/",  {
+        fetch("http://192.168.1.73/beer/",  {
             method:"GET"
 
         })
         
         .then(resp => resp.json())
         .then(data => {
-                setData(data.results)
+                setData(data.results)       
         })
        
-        .catch(error => Alert.alert("Error", error))
-
+        .catch(error => Alert.alert("Error", error.message))
+        
     }, [] )
 
     const renderData = (item) => {
         return(
-
+   
          <Card  style = {styles.cardStyle}>
     
               <Text style = {styles.textStyles}>{item.name}</Text>
-              <Image source={{uri: item.picture_url + '_100.png' }} style={{flex: 1,width: 100,height: 200,resizeMode: 'contain'}} />
+              <Text style = {styles.textStyles}>{item.beer_type}</Text>
+              <Image source={{uri: item.picture_url + '_100.png' }} style={styles.imageStyle} />
               <Text style = {styles.textStyles}>{item.container_type}</Text>
               <Text style = {styles.textStyles}>{item.volume + ' ml'}</Text>
-              <Text style = {styles.textStyles}>{item.alcohol_percentage + ' vol'}</Text>
+              <Text style = {styles.textStyles}>{item.alcohol_percentage + '% vol'}</Text>
     
         </Card>
+   
         )   
     }
     return (
  <View>
-
+    
          <FlatList 
              data = {data}
              renderItem = {({item}) => {
             return renderData(item)
           }}
-          keyExtractor = {item => `${item.id}`}
+          keyExtractor = {item => `${item.beer_ID}`}
      />
 
 </View>
@@ -61,7 +63,15 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
         alignContent: 'center',
         backgroundColor: 'grey',
-        borderRadius: 60
+        borderRadius: 60,
+       
+    },
+    imageStyle: {
+        flex: 1,
+        width: 100,
+        height: 200,
+        resizeMode: 'contain',
+        alignSelf: 'center'
     },
 
     textStyles : {
