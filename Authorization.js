0217d34939e4b309fd7bcef980541012d4fd67c4
sjogr3n <simon.sjogr3n@gmail.com>
2021-04-15 11:09:34 +0200

    Authorization-funktioner

---
+++ Authorization.js
@@ -0,0 +1,12 @@
+async function save(key, value) {
+    await SecureStore.setItemAsync(key, value);
+  }
+  
+  async function getValueFor(key) {
+    let result = await SecureStore.getItemAsync(key);
+    if (result) {
+      alert("🔐 Here's your value 🔐 \n" + result);
+    } else {
+      alert('No values stored under that key.');
+    }
+  }
