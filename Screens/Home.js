73f194e7f96621ea25099aa813e94083bbb26542
EgonAxel <egonaxelsson@hotmail.se>
2021-04-28 15:39:08 +0200

    Regnanimation på logga i hemskärm.

--- Screens/Home.js
+++ Screens/Home.js
@@ -1,12 +1,15 @@
-
-
 import React from 'react'
-import { StyleSheet, Text, View, Button, TouchableOpacity} from 'react-native';
+import { StyleSheet, Text, View, Button, TouchableOpacity, Image} from 'react-native';
 import * as SecureStore from 'expo-secure-store';
 import { Header } from 'react-native-elements';
+import axios from 'axios';
 import { Ionicons } from '@expo/vector-icons'; 
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+import MakeItRain from 'react-native-make-it-rain';
+import { render } from 'react-dom';
 
+// -- Make it rain ger loggor som faller ner, början på att få in de olika bilderna på öl
+// -- är gjord men inte klar och utkommenterad för tillfället. 
 
 //const AppButton istället för vanlig button för att få en redigerbar knapp som funkar på både ios o andriod.
 // länk för knappar: https://blog.logrocket.com/creating-custom-buttons-in-react-native/
@@ -18,22 +21,66 @@ const AppButton = ({ onPress, title }) => (
       <Text style={styles.appButtonText}>{title}</Text>
     </TouchableOpacity>
   );
-  async function save(key, value) {
-    await SecureStore.setItemAsync(key, value);
-  }
+  // async function save(key, value) {
+  //   await SecureStore.setItemAsync(key, value);
+  // }
   
-  async function getValueFor(key) {
-    let result = await SecureStore.getItemAsync(key);
-    if (result) {
-      return(result);
-    } else {
-      return(None);
-    }
-  }
+  // async function getValueFor(key) {
+  //   let result = await SecureStore.getItemAsync(key);
+  //   if (result) {
+  //     return(result);
+  //   } else {
+  //     return(None);
+  //   }
+  // }
 
 
-function Home({ navigation }) {
-  
+class Home extends React.PureComponent {
+  // constructor(props) {
+  //   super(props);
+ 
+  //   this.state = {
+  //     offset: 0,
+  //     beerImage: [],
+  //   };
+  // }
+
+  // fetchBeer = (offset) => {
+  //   getValueFor("Token").then((token) => {
+  //       axios
+  //       .get(`http://192.168.1.73:8000/beer/?limit=20&offset=${offset}`, {headers: { 'Authorization': `Token ` + token}}) //Här behävs din egen adress till APIn
+  //       .then(response => {
+  //         this.setState({
+  //           beerImage: this.state.beerImage.concat(response.data.results),
+  //         });
+  //       })
+  //       .catch(error => {
+  //         this.setState({error: error.message});
+  //       });
+  //   })
+  // }
+  // fetchMoreBeers = () => {
+  //   if (this.state.beerImage.length >= 20) {
+  //     this.setState(
+  //       prevState => ({
+  //         offset: prevState.offset + 20,
+  //       }),
+  //       () => {
+  //         this.fetchBeer(this.state.offset, this.state.searchText, this.state.orderingValue, this.state.beerType);
+  //       },
+  //     );
+  //   };
+  // }
+  // componentDidMount() {
+  //     this.fetchBeer(this.state.offset);
+  // }
+
+  // setBeerImage  = () => {
+
+  // }
+
+render(){
+    
 return (
 <SafeAreaProvider>
   <View>
@@ -52,27 +99,40 @@ return (
         <AppButton 
             title="Topplistan"
             onPress={() => {
-              getValueFor("Token").then((token) => {
-                console.log(token)
-              })
+              console.log( this.state.beerImage[1].picture_url + '_100.png')
             }}
           />
           <AppButton 
             title="Utforska"
             onPress={() =>
-              navigation.navigate('Utforska')
+              this.props.navigation.navigate('Utforska')
             }
           />
         <AppButton 
             title="Mitt konto"
             onPress={() =>
-              navigation.navigate('Mitt konto')
+              this.props.navigation.navigate('Mitt konto')
             }
           />
       </View> 
-    </View>
+	        <MakeItRain
+	          numItems={20}
+	          itemComponent={<Ionicons name="beer-outline"  size={50} />}
+           // itemComponent={<Image source = {{uri: "https://product-cdn.systembolaget.se/productimages/" + this.state.beerImage.beer + "/" + this.state.beerImage.beer + '_100.png' }}/>}
+           // itemComponent={<Image style = {{width: 100, height: 100, resizeMode: 'contain'}} source = {{uri: this.state.beerImage[1].picture_url + '_100.png' }}/>}
+            itemDimensions = {{ width: 50, height: 50 }}
+	          itemTintStrength={0}
+            fallSpeed = {10}
+            flipSpeed = {0}
+            horizSpeed = {20}
+            //flavor={"rain"}
+	        />
+    </View>    
   </SafeAreaProvider> 
+ 
     )
+    
+    }
 }
 
 const styles = StyleSheet.create({
