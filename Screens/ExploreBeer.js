6e0e2007494b54e127d573e8e1688618caef814c
EgonAxel <egonaxelsson@hotmail.se>
2021-05-18 11:40:26 +0200

    Placeholder namn på 'ingen' sortering/filter

--- Screens/ExploreBeer.js
+++ Screens/ExploreBeer.js
@@ -96,7 +96,7 @@ class Beers extends React.PureComponent {
   fetchBeer = (offset, searchText, orderingValue, beerType) => {
     getValueFor("Token").then((token) => {
       axios
-      .get(`http://127.0.0.1:8000/beer/?limit=20&offset=${offset}&search=${searchText.replace(' ','+')}&ordering=${orderingValue}&beer_type=${beerType}`, {headers: { 'Authorization': `Token ` + token}}) //Här behävs din egen adress till APIn
+      .get(`http://192.168.1.73:8000/beer/?limit=20&offset=${offset}&search=${searchText.replace(' ','+')}&ordering=${orderingValue}&beer_type=${beerType}`, {headers: { 'Authorization': `Token ` + token}}) //Här behävs din egen adress till APIn
       .then(response => {
         this.setState({
           beers: this.state.beers.concat(response.data.results),
@@ -214,7 +214,7 @@ class Beers extends React.PureComponent {
             <RNPickerSelect style={pickerSelectStyles}
               useNativeAndroidPickerStyle={false}
               placeholder={{
-                label: '', 
+                label: 'Alla', 
                 inputLabel: 'Ölsort',
                 value: ''
               }}
@@ -234,7 +234,7 @@ class Beers extends React.PureComponent {
             <RNPickerSelect style={pickerSelectStyles}
               useNativeAndroidPickerStyle={false}
               placeholder={{
-                label: '',
+                label: 'Ingen',
                 inputLabel: 'Sortering',
                 value: ''
               }}
